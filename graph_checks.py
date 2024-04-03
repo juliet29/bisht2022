7626e1b99c6a0d4d2840cb2f16c770027486af1c
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2024-04-03 12:16:05 -0700

    thinking ..

--- graph_checks.py
+++ graph_checks.py
@@ -23,11 +23,12 @@ def check_shared_neighbour(G: nx.Graph, cycle):
         return True
     
 
-def seperating_triangle_check(G):
+def check_seperating_triangle(G):
     l3_cycles = sorted(nx.simple_cycles(G, 3))
     m = len(list(G.edges))
     n = len(list(G.nodes))
     # if self.DEBUG:
-    ic(len(l3_cycles), m, n, m - n + 1)
-    assert len(l3_cycles) == m - n + 1, f"{len(l3_cycles), m, n, m-n+1}"
+    # ic(len(l3_cycles), m, n, m - n + 1)
+    ic(f"\n {len(l3_cycles)} three cycles ?= {m - n + 1}, where m={m}, n={n}")
+    return False
 
