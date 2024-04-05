19c7f672a3254266938023c14c408fb2abc2b905
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2024-04-05 09:43:57 -0700

    some progress on locating nodes

---
+++ four_complete_coordinates.py
@@ -0,0 +1,93 @@
+from helpers import *
+
+from shapely import LineString, Point
+
+
+class FourCompleteCoordinates:
+    def __init__(self, GraphData: GraphData, path: list, distance:float = 0.5) -> None:
+        self.data = GraphData
+        self.G = GraphData.G
+        self.embed = GraphData.embed
+        self.path = path
+        self.distance = distance
+
+        self.create_corner_node_location()
+
+    # MARK: action begins
+    def create_corner_node_location(self):
+        self.get_slopes()
+        self.choose_best_slope()
+        self.choose_best_midpoint()
+        self.define_corner_node_axis()
+        self.corner_node_location = (self.orthog_line.centroid.x, self.orthog_line.centroid.y)
+
+
+    def get_slopes(self):
+        self.boundary_embed = get_dict_subset(self.data.embed, self.path)
+        embed_vals = [tuple(v) for v in self.boundary_embed.values()]
+        embed_keys = [k for k in self.boundary_embed.keys()]
+
+        self.slopes = []
+        self.nodes = []
+
+        for val, key in zip(embed_vals[1:], embed_keys[1:]):
+            slope, b = find_line_through_points((embed_vals[0], val))
+            self.slopes.append(np.round(slope, decimals=9))
+            self.nodes.append((embed_keys[0], key))
+        self.slopes = np.array(self.slopes)
+
+
+    def choose_best_slope(self):
+        # if no slopes are most freq, just get the first
+        if len(np.unique(self.slopes)) == 1:
+            self.most_freq_slope = self.slopes[0]
+        else:
+            _, frequencies = np.unique(self.slopes, return_counts=True)
+
+            most_freq_index = np.argmax(frequencies)
+            self.most_freq_slope = self.slopes[most_freq_index]
+
+            assert(self.most_freq_slope), f"most freq index was {most_freq_index}"
+
+        
+    def choose_best_midpoint(self):
+        # may have multiple lines with same slope, get midpoint of longest
+        valid_slope_ix = np.where(self.slopes == self.most_freq_slope)[0]
+
+        line = LineString(((0, 0), (0, 0)))
+
+        for ix in valid_slope_ix:
+            u, v = self.nodes[ix]
+            u_coord = self.boundary_embed[u]
+            v_coord = self.boundary_embed[v]
+
+            l = LineString((u_coord, v_coord))
+            if l.length > line.length:
+                line = l
+
+        self.midpoint = line.centroid
+
+    def define_corner_node_axis(self):
+        self.orthog_line = line_from_point_and_slope(
+            self.midpoint, orthogonal_slope(self.most_freq_slope), self.distance
+        )
+
+
+
+# MARK: helpers
+def line_from_point_and_slope(point, slope, length=10):
+    # Calculate the second point using the given point and slope
+    x2 = point.x + length
+    y2 = point.y + length * slope
+    second_point = Point(x2, y2)
+
+    # Create a LineString from the two points
+    line = LineString([point, second_point])
+    return line
+
+
+def orthogonal_slope(slope):
+    if slope != 0:
+        return -1 / slope
+    else:
+        return 0
---
+++ four_complete_locations.py
@@ -0,0 +1,65 @@
+from helpers import *
+
+from four_complete_coordinates import *
+
+
+class FourCompleteLocations:
+    def __init__(self, GraphData: GraphData, boundary:list, paths:list) -> None:
+        self.data = GraphData
+        self.G = GraphData.G
+        self.embed = GraphData.embed
+        self.boundary = boundary
+        self.paths  = paths
+
+        self.corner_node_data = []
+
+    def run(self):
+        self.get_corner_node_coordinates()
+        self.assign_cardinal_directions()
+        self.embed_corner_nodes()
+
+
+    def get_corner_node_coordinates(self):
+        for ix, p in enumerate(self.paths):
+            l = FourCompleteCoordinates(self.data, p)
+            self.corner_node_data.append(CornerNode(
+                location=l.corner_node_location,
+                index = len(self.G.nodes) + ix,
+                neighbour_indices = p 
+                ))
+
+    def assign_cardinal_directions(self):
+        coords = [i.location for i in self.corner_node_data]
+        directions = assign_directions(coords)
+        for k,v in directions.items():
+            ix = coords.index(v)
+            self.corner_node_data[ix].name = k
+
+
+    def embed_corner_nodes(self):
+        for v in self.corner_node_data:
+            # update graph edges
+            new_edges = []
+            for node in v.neighbour_indices:
+                new_edges.append((v.index, node))
+                self.G.add_edges_from(new_edges)
+        
+            # update the embedding 
+            self.embed[v.index] = np.array(v.location)
+
+
+    def connect_outer_nodes(self):
+        # TODO move to own four connect class?
+        ix = self.get_node_index  # create alias
+        edges = [
+            (ix("south"), ix("east")),
+            (ix("east"), ix("north")),
+            (ix("north"), ix("west")),
+            (ix("west"), ix("south")),
+            (ix("south"), ix("north")),
+        ]
+        self.G.add_edges_from(edges)
+
+    def get_node_index(self, key):
+        dict_key = get_key_by_value(self.corner_node_data, key, object=True)
+        return self.corner_node_data[dict_key].index
