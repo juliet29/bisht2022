7777d12c04f54c0f09b873f4eef06f0e1c2c5352
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2024-06-07 11:08:22 -0700

    update

--- scripts/graph_generators/simple_four_con.py
+++ scripts/graph_generators/simple_four_con.py
@@ -1,27 +1,33 @@
-from helpers import nx, np
+import networkx as nx
+import numpy as np
 from helpers_classes import CornerNode, CardinalDirections, GraphData
 
+
 def simple_four_con_graph():
-    pos = [(-3,0), (0,4),(0,8), (3,4), (6,2), (3,0), (0,-4), (0,0) ]
-    outer_edges = [(0,1), (1,2), (2,3), (3,4), (4,5), (5,6), (6,7), (7,0)]
-    inner_edges = [(7,1), (1,3), (3,5), (5,7), (1,5)]
-    edges_connecting_outer = [(0,2), (2,4), (4,6), (6,0)]
-    
+    pos = [(-3, 0), (0, 4), (0, 8), (3, 4), (6, 2), (3, 0), (0, -4), (0, 0)]
+    outer_edges = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 0)]
+    inner_edges = [(7, 1), (1, 3), (3, 5), (5, 7), (1, 5)]
+    edges_connecting_outer = [(0, 2), (2, 4), (4, 6), (6, 0)]
+
     edges = outer_edges + inner_edges + edges_connecting_outer
     G = nx.Graph()
     G.add_edges_from(edges)
-    embed = {k:np.array(v) for (k,v) in enumerate(pos)}
+    embed = {k: np.array(v) for (k, v) in enumerate(pos)}
 
     return G, embed
 
+
 def name_corner_nodes():
-    corner_nodes = [CornerNode(index=0, name=CardinalDirections.WEST), 
-    CornerNode(index=2, name=CardinalDirections.NORTH), 
-    CornerNode(index=4, name=CardinalDirections.EAST), 
-    CornerNode(index=6, name=CardinalDirections.SOUTH), ]
+    corner_nodes = [
+        CornerNode(index=0, name=CardinalDirections.WEST),
+        CornerNode(index=2, name=CardinalDirections.NORTH),
+        CornerNode(index=4, name=CardinalDirections.EAST),
+        CornerNode(index=6, name=CardinalDirections.SOUTH),
+    ]
     corner_node_dict = {k: v for k, v in zip(list(range(4)), corner_nodes)}
 
     return corner_node_dict
+
 
 def create_graph():
     G, embed = simple_four_con_graph()
@@ -29,5 +35,6 @@ def create_graph():
     graph_data = GraphData(G, embed, corner_node_dict)
 
     return graph_data
+
 
 graph_data = create_graph()
